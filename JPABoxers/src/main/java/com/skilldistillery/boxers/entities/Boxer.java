ccface67b6dcb7c435e203b3130239d2f0aca795
Rahil Ali <rahtrill@gmail.com>
2022-04-16 17:12:09 -0600

    Add boxer entities and updated db columns, add entries to db

--- JPABoxers/src/main/java/com/skilldistillery/boxers/entities/Boxer.java
+++ JPABoxers/src/main/java/com/skilldistillery/boxers/entities/Boxer.java
@@ -18,8 +18,37 @@ public class Boxer {
 
 	@Column(name = "last_name")
 	private String lastName;
-
+	
+	@Column(name = "height_inches")
+	private int height;
+	
+	@Column(name = "weight_class")
+	private String weightClass;
+	
+	private String nationality;
+	
+	@Column(name = "reach_inches")
+	private double reach;
+	
+	private int knockouts;
+	
+	@Column(name = "total_fights")
+	private int totalFights;
+	
 	public Boxer() {
+	}
+
+	public Boxer(int id, String firstName, String lastName, int height, String weightClass, String nationality,
+			double reach, int knockouts, int totalFights) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.height = height;
+		this.weightClass = weightClass;
+		this.nationality = nationality;
+		this.reach = reach;
+		this.knockouts = knockouts;
+		this.totalFights = totalFights;
 	}
 
 	public int getId() {
@@ -48,7 +77,57 @@ public class Boxer {
 
 	@Override
 	public String toString() {
-		return "Boxer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+		return "Boxer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", height=" + height
+				+ ", weightClass=" + weightClass + ", nationality=" + nationality + ", reach=" + reach + ", knockouts="
+				+ knockouts + ", totalFights=" + totalFights + "]";
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public String getWeightClass() {
+		return weightClass;
+	}
+
+	public void setWeightClass(String weightClass) {
+		this.weightClass = weightClass;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public double getReach() {
+		return reach;
+	}
+
+	public void setReach(double reach) {
+		this.reach = reach;
+	}
+
+	public int getKnockouts() {
+		return knockouts;
+	}
+
+	public void setKnockouts(int knockouts) {
+		this.knockouts = knockouts;
+	}
+
+	public int getTotalFights() {
+		return totalFights;
+	}
+
+	public void setTotalFights(int totalFights) {
+		this.totalFights = totalFights;
 	}
 
 }
