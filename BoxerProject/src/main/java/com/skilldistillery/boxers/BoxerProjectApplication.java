f6addcb56df9c8ac4faa7bbf26e896c0c4eedad1
Rahil Ali <rahtrill@gmail.com>
2022-04-15 19:30:35 -0600

    Start Boot MVC project, add JPA project dependency

---
+++ BoxerProject/src/main/java/com/skilldistillery/boxers/BoxerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.boxers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxerProjectApplication.class, args);
+	}
+
+}
---
+++ BoxerProject/src/test/java/com/skilldistillery/boxers/BoxerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.boxers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
